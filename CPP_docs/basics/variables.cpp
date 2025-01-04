e7967c402d353dd3d944168faafdce11467b005e
Lance <lancefinke@icloud.com>
2025-01-04 17:40:52 -0600

    feat:variable files

--- CPP_docs/basics/variables.cpp
+++ CPP_docs/basics/variables.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 
-void Log(const char* message);
+
 
 int main() {
 
 
-	Log("Hello World");
-	std::cin.get();
+	int var = 8;//signed int: -2b -> 2b
 
+	unsigned int num = 10;//can store value twice of a signed but
+
+
+	std::cout << var << std::endl;
+	std::cin.get();
 }
