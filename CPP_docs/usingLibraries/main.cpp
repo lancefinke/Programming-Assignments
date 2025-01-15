79e52a4cd844afec11607f78c1c72bce8fe50371
Lance <lancefinke@icloud.com>
2025-01-15 15:03:50 -0600

    feat:implemented glfw

---
+++ CPP_docs/usingLibraries/main.cpp
@@ -0,0 +1,36 @@
+#define GLFW_INCLUDE_NONE
+#include<iostream>
+#include <GLFW/glfw3.h>
+#include <thread>
+#include <chrono>
+
+
+void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+
+
+int main() {
+
+	if (!glfwInit())
+	{
+		std::cout << "Init failed" << std::endl;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "window failed" << std::endl;
+	}
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+	
+	glfwTerminate();
+
+
+
+	
+	std::cin.get();
+}
